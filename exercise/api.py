dcbb6de6a5e9a46dbb383bb6089e0044311819c6
Sophie <sophie.deragobian@gmail.com>
2023-07-20 15:38:26 +0200

    [Dev] add unique id and catch errors

--- exercise/api.py
+++ exercise/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from threading import Thread
+import uuid
 
 import utils
 from device import Device
+from exercise.errors import ErrorDeviceAlreadyExists, ErrorDeviceNotFound
 
 
 class API:
@@ -21,12 +23,28 @@ class API:
 
     @app.route("/device", methods=["POST"])
     def add_device():
-        data = request.json
-        device = Device(**data)
-        utils.create_device(device)
-        return "", 200
+        try:
+            data = request.json
+            # create unique id
+            unique_id = str(uuid.uuid4())
+            device = Device(id=unique_id, **data)
+            try:
+                utils.create_device(device)
+                return unique_id, 200
+
+            except ErrorDeviceAlreadyExists:
+                return "", 403
+        except:
+            return "", 500
 
     @app.route("/device/<id>", methods=["DELETE"])
     def remove_device(id: str):
-        utils.remove_device(id)
-        return "", 204
+        try:
+            try:
+                utils.remove_device(id)
+                return f"delete : {id}", 204
+
+            except ErrorDeviceNotFound:
+                return "", 404
+        except:
+            return "", 500
