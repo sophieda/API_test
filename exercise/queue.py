1856817e80c713e47c196ed59ed7e22606e4b279
Sophie <sophie.deragobian@gmail.com>
2023-07-23 16:16:33 +0200

    [Dev] Decrease complexity of max priority job
    Add a new attribute in Job class.
    Update the max priority only when it is necessary.

--- exercise/queue.py
+++ exercise/queue.py
@@ -1,21 +1,29 @@
 class PriorityQueue:
-    queue = {}
+    queue: dict = {}
+    max_priority: int = -1
 
-    def push(self, item, priority):
+    def push(self, item, priority: int):
         if priority in self.queue:
             self.queue[priority].append(item)
         else:
             self.queue[priority] = [item]
 
+        # update the max priority if it necessary
+        if self.max_priority < priority:
+            self.max_priority = priority
+
     def pop(self):
-        # TODO : keep historic higher priority to reduce time complexity
-        index_priority = max(list(self.queue.keys()))
 
         # first item = oldest item in the queue
-        pop_item = self.queue[index_priority].pop(0)
+        pop_item = self.queue[self.max_priority].pop(0)
 
-        # if it was the last item in the priority, delete
-        if not self.queue[index_priority]:
-            del self.queue[index_priority]
-
+        # if it was the last item in the priority, delete and update the max_priority
+        if not self.queue[self.max_priority]:
+            del self.queue[self.max_priority]
+            if self.queue:
+                # if queue not empty, take the new max
+                self.max_priority = max(list(self.queue.keys()))
+            else:
+                # if queue empty, max priority has default value
+                self.max_priority = -1
         return pop_item
